b2686b925e44144d3b4ba37a6b932820d55fcd5a
Tony Nguyen <anhhao1999@gmail.com>
2020-01-01 21:16:54 -0800

    Feed forward, normalization and positional encoding

---
+++ modules/CoreLayers.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+from utils.weights_utils import initialize_xavier_weights
+
+
+class FeedForward(nn.Module):
+    """
+    A fully connected feed forward layer that is attached at the end of every attention module. This feed-forward
+    network is applied to each position separately and identically.
+    """
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        """
+        Inputs:
+            d_model: the dimensionality of the outputs of the architecture <hyperparameter>
+            d_ff: the dimensionality of the hidden state of the feed forward network <hyperparameter>
+            dropout: the rate of dropout for regularization
+        """
+        super(FeedForward, self).__init__()
+
+        # the feed forward network consists of 2 transformation, essentially, the formula which we wish to implement
+        # is FF(x) = ReLU(0, xW1 + b1)W2 + b2 -- so we first apply a linear transformation, apply the ReLU activation
+        # function, and then apply a second linear transformation
+        first_transformation = nn.Linear(d_model, d_ff)
+        second_transformation = nn.Linear(d_ff, d_model)
+        initialize_xavier_weights(first_transformation)
+        initialize_xavier_weights(second_transformation)
+
+        # dropout for regularization
+        dropout = nn.Dropout(dropout)
+
+        self.feed_forward = nn.Sequential(
+            first_transformation,
+            nn.ReLU(inplace=True),
+            second_transformation,
+            dropout
+        )
+
+    def forward(self, attention_word_embeddings):
+        """
+        Apply the feed forward network onto the given inputs.
+
+        Input:
+            attention_word_embeddings: the resulting matrix from performing the multi-head mechanism in both the encoder
+            and decoder.
+
+            The shape of this matrix should be (batch_size, seq_len, d_model)
+
+        Outputs:
+            The computed values passed through the feed forward layer.
+        """
+        # we have declared the sequential module that is the core operations we require to perform the feed forward
+        # action, and so we simply need to apply the sequential network onto our inputs
+        return self.feed_forward(attention_word_embeddings)
+
+
+class LayerNormalization(nn.Module):
+    """
+    The module which implements layer normalization. The intuition behind layer normalization extends that of batch
+    normalization. The distribution of training data points can and will vary with every batch, and therefore, causes
+    the neural network to continually adapt to these differences to learn the data well. With more and more layers in
+    the neural network, higher order interactions can increase the loss as training progresses unless a very small
+    learning rate is used. Thus, we wish to normalize the mean and variance of each batch to 0 and 1 respectively so
+    that the neural network has less complications to deal with, and a higher learning rate can be used to speed up
+    training.
+
+    The limitation of batch normalization is that this means that we cannot use a small batch size because then the
+    normalization would not be effective. This means that there is a lower limit to the batch size, and thus increases
+    memory consumption during training. We also know that small batch size is not a good setting for online learning, or
+    algorithms that are very sensitive to noises. Instead of normalizing all activations within a batch to follow the
+    same mean and variance, we normalize it along all feature dimensions.
+
+    We essentially need to learn 2 parameters, gamma and beta, as essentially, we are performing a covariate shift.
+    """
+    def __init__(self, d_model, epsilon=1e-6):
+        """
+        Inputs:
+            d_model: the dimensionality of the outputs <hyperparameter>
+            epsilon: a float which is used for numerical stability in the case that variance is 0
+        """
+        super(LayerNormalization, self).__init__()
+
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.epsilon = epsilon
+
+    def forward(self, activations):
+        """
+        Inputs:
+            activations: the activations outputted by each module of the neural network that needs to be normalized.
+
+            The shape of the activations should have shape (batch_size, seq_len, d_model)
+
+        Outputs:
+            The shifted normalized activations for the layer requiring normalization.
+        """
+        # we want to get the mean and variance of each feature within this batch, since the input has shape
+        # batch_size, seq_len, d_model, the last dimension is the dimension of the features for each word; we also set
+        # keepdims to True so we can perform subtraction between the original activations tensor to the mean tensor
+        feature_mean = activations.mean(dim=-1, keepdims=True)
+
+        # we compute the variance
+        feature_variance = ((activations - feature_mean) ** 2).mean(dim=-1, keepdims=True)
+
+        # we now center the activation
+        centered_activations = activations - feature_mean
+
+        # each normalized activation is subtracted by its corresponding mean (mean of the feature dimension it is from)
+        # and divided by standard deviation -- here the standard deviation is computed as variance + epsilon square
+        # rooted because we want to avoid having the standard deviation being zero as we cannot divide by 0
+        feature_st_dev = (feature_variance + self.epsilon).sqrt()
+        normalized_activations = centered_activations / feature_st_dev
+
+        return self.gamma * normalized_activations + self.beta
---
+++ modules/PositionalEncoding.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+    """
+    The implementation of positional encoding which injects information of the relative and absolute positioning of
+    words within sentences.
+    """
+    def __init__(self, d_model, set_seq_len=None, device=torch.device("cpu")):
+        """
+        Input:
+            d_model: the dimensionality of the outputs <hyperparameter>
+        """
+        super(PositionalEncoding, self).__init__()
+        self.d_model = d_model
+
+        # here we provide the option of pre-computing our positional encoding tensor
+        # the positional encoding information is independent from the semantic meaning of any sentence, so in other
+        # words, it is independent from the word embeddings tensor, that is, we can precompute the necessary positions
+        # and simply use this stored information as needed, however, we need to compute positional encoding for every
+        # word position in every training example, we need to ensure that set_seq_len >= length of the longest training
+        # example within our training dataset -- to optimize for space, we need set_seq_len to be exactly that length
+        # and when we use our precomputed positional encoding, we can just index the stored tensor up to the padded
+        # seq_len of the batch
+        self.precomputed_pos_encoding = None
+        if set_seq_len is not None:
+            self.precomputed_pos_encoding = self.get_positional_encoding(set_seq_len, device=device)
+
+    def compute_angles(self, positions, dimensions):
+        """
+        Compute the inner formula which the sin and cos functions will be called on.
+        Input:
+            positions: the word embeddings matrix that does not have positional encoding information
+
+        Output:
+            The computed formula for each position: pos/(10000^{2i/d_model})
+        """
+        return positions / (10000 ** (2 * dimensions / self.d_model))
+
+    def get_positional_encoding(self, seq_len, device=torch.device("cpu")):
+        """
+        Compute the full formula which implements the positional information which will be added into our original word
+        embeddings.
+
+        Input:
+            seq_len: the length of the longest example within our batch
+
+        Output:
+            The computed positional encoding for each position and dimensions
+        """
+        # we need to generate the positions tensor, which goes from 0 to seq_len - 1
+        positions = torch.arange(seq_len, dtype=torch.float)[:, None]
+
+        # we need to generate the dimensions tensor, which goes from 0 to d_model - 1
+        dimensions = torch.arange(self.d_model, dtype=torch.float)[None, :]
+
+        # note: we make positions of shape (seq_len, 1) and dimensions of shape (1, d_model), so when we multiply
+        # the 2, we get a matrix that computes the formula for every pair of (pos, i) where pos is a position in
+        # the sentence and i is a dimension
+
+        # get the inner part of the computation (the part which we call sin and cos on)
+        angles = self.compute_angles(positions, dimensions)
+
+        # we apply sin on the even indices and cos on the odd indices
+        # using torch's indexing syntax, to get the even indices, we start at index 0 and take every second element,
+        # to get the odd indices, we start at index 1 and take every second element
+
+        # note that we want to index into the "i" part, meaning that the decision of whether to apply a sin or a cos
+        # to the formula computed from angles depends on the dimension which a value lies in, not the position
+        # so we index into the second dimension of the tensor
+        sines = torch.sin(angles[:, 0::2])
+        cosines = torch.cos(angles[:, 1::2])
+
+        # since we split up the tensor using the second dimension, here, we concatenate them back along the second
+        # dimension
+
+        # we want to add another dimension so when we add this positional information to our word embedding, it is added
+        # to all training examples
+        return torch.cat((sines, cosines), dim=1)[None, :].to(device)
+
+    def forward(self, batch_word_embeddings):
+        """
+        Add the current word embeddings to the computed positional encoding formula to incorporate information regarding
+        the words' relative and absolute positions within the sequences.
+
+        Input:
+            batch_word_embedding: A matrix of word embeddings for each training example in the batch
+
+            The shape of the input should be (batch_size, seq_len, d_model)
+
+        Output:
+            The word embeddings added with the computed positional encoding.
+        """
+        seq_len = batch_word_embeddings.size(1)
+
+        # if we already precomputed the positional encoding as described above, then just add it to the word embeddings
+        try:
+            return batch_word_embeddings + self.precomputed_pos_encoding[:, :seq_len, :]
+
+        # if not, we need to compute the positional encoding
+        except TypeError:
+            device = batch_word_embeddings.device
+            positional_encoding = self.get_positional_encoding(seq_len, device=device)
+            return batch_word_embeddings + positional_encoding
